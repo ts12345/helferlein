f31ea421e7839b37b14932437a97883cca301672
ts <t@s.de>
2019-04-02 19:24:35 +0200

    #03 getter fuer Name und reformatiert

--- lib/model/Vector.dart
+++ lib/model/Vector.dart
@@ -8,6 +8,17 @@ class Vector {
   Vector(this.x, this.y, this.z, this.name);
 
   String getString() {
-    return name + "(" + x.toString() + "|" + y.toString() + "|" + z.toString() + ")^T";
+    return name +
+        "(" +
+        x.toString() +
+        "|" +
+        y.toString() +
+        "|" +
+        z.toString() +
+        ")^T";
+  }
+
+  String getName() {
+    return name;
   }
 }
