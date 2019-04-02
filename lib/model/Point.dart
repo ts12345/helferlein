31fb8eb0e818bea3eaf3d5289a453a073f03f2a1
ts <t@s.de>
2019-04-02 19:46:34 +0200

    #01 Aenderung Darstellung Punkt; ganzzahlige Werte ohne Komma

--- lib/model/Point.dart
+++ lib/model/Point.dart
@@ -5,11 +5,18 @@ class Point {
 
   String name;
 
-
   Point(this.x, this.y, this.z, this.name);
 
   String getString() {
-    return name + "(" + x.toString() + "|" + y.toString() + "|" + z.toString() + ")";
+    return name + "(" + value(x) + "|" + value(y) + "|" + value(z) + ")";
+  }
+
+  String value(double value) {
+    int a = value.toInt();
+    if (value / a.toDouble() == 1)
+      return a.toString();
+    else
+      return value.toString();
   }
 
   String getName() {
